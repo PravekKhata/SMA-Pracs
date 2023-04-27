2b78f2d1914e5027bcea34c18ecc1600aa325f03
PravekKhata <79007729+PravekKhata@users.noreply.github.com>
2023-04-27 07:12:45 +0530

    changed to Spacy library for preprocessing

--- Sentiment Analysis/sentiment.py
+++ Sentiment Analysis/sentiment.py
@@ -5,29 +5,40 @@ import matplotlib.pyplot as plt
 from textblob import TextBlob
 from nltk import PorterStemmer,word_tokenize
 from nltk.corpus import stopwords
+import spacy
 
 df = pd.read_csv('D:\Desktop\SMA\Practicals\SMA Pycharm Pracs\sma_dataset.csv')
 print(df)
 
+nlp = spacy.load('en_core_web_sm')
 # preprocessing
 def preprocess(text):
 
-    text = text.lower()
+    # text = text.lower()
+    #
+    # # Removing punctutions
+    # text = text.translate(str.maketrans('','',string.punctuation))
+    #
+    # # Tokenizing words and Removing stopwords
+    # words = nltk.word_tokenize(text)
+    # stop_words = set(stopwords.words(('english')))
+    # words = [word for word in words if word not in stop_words]
+    #
+    # # Stem the words
+    # stemmer = PorterStemmer()
+    # words = [stemmer.stem(word) for word in words]
+    doc = nlp(str(text).lower())
 
-    # Removing punctutions
-    text = text.translate(str.maketrans('','',string.punctuation))
+    tokens = [token for token in doc]
+    tokens = [token for token in doc if not token.is_stop and not token.is_punct and not token.is_digit]
+    print(tokens)
+    print()
 
-    # Tokenizing words and Removing stopwords
-    words = nltk.word_tokenize(text)
-    stop_words = set(stopwords.words(('english')))
-    words = [word for word in words if word not in stop_words]
-
-    # Stem the words
-    stemmer = PorterStemmer()
-    words = [stemmer.stem(word) for word in words]
+    # finding lemma
+    tokens = [token.lemma_ for token in tokens]
 
     # Join the words back into a string
-    text = ' '.join(words)
+    text = ' '.join(tokens)
 
     return text
 
@@ -73,3 +84,17 @@ for items in df['Sentiment_polarity']:
 
 df['Sentiment'] = sentiment_list
 print("\n\n",df[['Clean Tweet','Sentiment']].head(5))
+
+sentiment_counts = df['Sentiment'].value_counts()
+
+# plotting a bar graph
+plt.bar(sentiment_counts.index,sentiment_counts.values)
+plt.title('Sentiment Distribution')
+plt.xlabel('Sentiment')
+plt.ylabel('Count')
+plt.show()
+
+# Plotting a pie graph
+plt.pie(sentiment_counts,autopct='%1.1f%%',startangle=90,labels=sentiment_counts.index)
+plt.title('Sentiment Distribution')
+plt.show()
